fcd7a2d0ab193c280b6ba7d8e07e809e648bfa4a
Arash <104251944+Rash-1@users.noreply.github.com>
2022-07-14 21:21:00 +0430

    Update CategoryRequest.php
    request rules change

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -24,7 +24,7 @@ class CategoryRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|max:255|min:3|unique:App\Models\Category,name'
+            'name' => 'required|unique:App\Models\Category,name'
         ];
     }
 }
