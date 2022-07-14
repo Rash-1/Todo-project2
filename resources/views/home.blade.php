0b66313548b85bcbb9c55f4b2afb3a52dadbb12f
Arash <104251944+Rash-1@users.noreply.github.com>
2022-07-14 21:16:09 +0430

    home page changed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,7 @@
 <main class="px-3">
     <div class="border border-primary border-2 rounded-pill text-gray-800">
         <h1>Hi</h1>
-        <h2>I'm Rash</h2>
-        <h3>And</h3>
-        <h4>This is my Todo app</h4>
+        <h2>This Is You And My TODO App</h2>
     </div>
 </main>
 @endsection
